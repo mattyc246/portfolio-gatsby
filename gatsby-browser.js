3cd9e90b67302950854995480a067ed7b8a2c72a
Matthew Cross <mattyc246@gmail.com>
2023-02-06 16:38:36 +0800

    update to gatsby v5

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,6 @@
-import React from 'react';
 import './src/styles/global.css';
 import 'prism-themes/themes/prism-one-dark.css';
 
-import Layout from './src/components/Layout';
-
 export const wrapRootElement = ({ element }) => {
-  return <Layout>{element}</Layout>;
+  return element;
 };
